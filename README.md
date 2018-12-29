210b4787f2eeb1d4061bdf3ec0da6ba19da44d5f
Joseph Chow <joe@xoio.co>
2018-12-29 09:43:29 -0800

    - add shadow mapping + SuperForumla samples. - minor updates to style - update list of samples

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Notes
 ====
 * For every new demo added don't forget to run `scripts/generateDemoData.js`
 * Deployment alias is https://cinder-emscripten.now.sh/
+* If demo has special needs or if there is another message you want to write - you can add a note by adding a `notes.json` file and 
+add the key "notes".
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,14 @@ export default () =>
         <div id="copy">
             <h1>Cinder Emscripten </h1>
             <h3>These are just a small sampling of what's possible with Cinder and Emscripten</h3>
+
+            <br/>
+            <p>More information:</p>
+
+            <ul>
+                <li><a href="https://libcinder.org/docs/guides/emscripten/index.html">Emscripten Guide</a></li>
+                <li><a href="http://libcinder.org" target="_blanke">Cinder</a></li>
+            </ul>
         </div>
         <div id="demo-items">
 
